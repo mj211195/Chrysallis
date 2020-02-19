4716716a295ce659b455dd2d381a30a43223f63a
Juan Leon <carlos25840@hotmail.com>
2020-02-19 13:54:11 +0100

    filtrar fecha

--- Chrysallis/FormEventos.cs
+++ Chrysallis/FormEventos.cs
@@ -27,7 +27,15 @@ namespace Chrysallis
 
         private void RefrescarDatos()
         {
-            List<eventos> eventos = EventoORM.SelectAllEventos();
+            List<eventos> eventos;
+            if ((bool)FormLogin.socioLogin.estatal)
+            {
+                eventos = EventoORM.SelectAllEventos();
+            }
+            else
+            {
+                eventos = EventoORM.SelectAllEventosByComunidad((int)FormLogin.socioLogin.id_comunidad);
+            }
             if (eventos == null)
             {
                 DialogResult result = MessageBox.Show("Error al acceder a la BD.\nSe procede a cerrar el programa.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -41,6 +49,44 @@ namespace Chrysallis
                 bindingSourceEventos.DataSource = eventos;
             }
 
+        }
+
+        private void dateTimePickerFiltroFecha_ValueChanged(object sender, EventArgs e)
+        {
+            buscarPorFecha();
+        }
+
+        private void dateTimePickerHasta_ValueChanged(object sender, EventArgs e)
+        {
+            buscarPorFecha();
+        }
+
+        public void buscarPorFecha()
+        {
+            List<eventos> eventos;
+            String desde = dateTimePickerDesde.Value.ToString("yyyy-MM-dd");
+            String hasta = dateTimePickerHasta.Value.ToString("yyyy-MM-dd");
+
+            if ((bool)FormLogin.socioLogin.estatal)
+            {
+                eventos = EventoORM.SelectAllEventosByFecha(DateTime.Parse(desde), DateTime.Parse(hasta));
+            }
+            else
+            {
+                eventos = EventoORM.SelectAllEventosByComunidadFecha((int)FormLogin.socioLogin.id_comunidad, DateTime.Parse(desde), DateTime.Parse(hasta));
+            }
+            if (eventos == null)
+            {
+                DialogResult result = MessageBox.Show("Error al acceder a la BD.\nSe procede a cerrar el programa.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (result == DialogResult.OK || result == DialogResult.Abort)
+                {
+                    Application.Exit();
+                }
+            }
+            else
+            {
+                bindingSourceEventos.DataSource = eventos;
+            }
         }
     }
 }
