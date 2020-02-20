0f358998380767dc38694303183b17231ea93dcd
Juan Leon <carlos25840@hotmail.com>
2020-02-20 13:44:17 +0100

    FOrmMenu

--- Chrysallis/Program.cs
+++ Chrysallis/Program.cs
@@ -16,7 +16,7 @@ namespace Chrysallis
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormEvento());
+            Application.Run(new FormMenu());
         }
     }
 }
