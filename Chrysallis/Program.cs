ad7d8defc12bfb65ff750b17e502a3e916e0e595
Juan Leon <carlos25840@hotmail.com>
2020-02-20 13:40:09 +0100

    prueba

--- Chrysallis/Program.cs
+++ Chrysallis/Program.cs
@@ -16,7 +16,7 @@ namespace Chrysallis
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMenu());
+            Application.Run(new FormEvento());
         }
     }
 }
