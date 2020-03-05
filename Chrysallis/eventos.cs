0323e844627d5148c192cde90c28154363bd3dc1
María José Benites Gaitán <mariajosebg95@gmail.com>
2020-03-05 09:50:47 +0100

    Modelo actualizado

--- Chrysallis/eventos.cs
+++ Chrysallis/eventos.cs
@@ -27,8 +27,11 @@ namespace Chrysallis
         public string ubicacion { get; set; }
         public System.TimeSpan hora { get; set; }
         public Nullable<System.DateTime> fechaLimite { get; set; }
-        public Nullable<int> numAsistentes { get; set; }
+        public int numAsistentes { get; set; }
         public int id_comunidad { get; set; }
+        public string nombre { get; set; }
+        public byte[] imagen { get; set; }
+        public string descripcion { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<asistir> asistir { get; set; }
